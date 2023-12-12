b1ef44d5e5fdf3a724a5ced2dda86c5dc8c6773c
arinka <arinkaheaven@gmail.com>
2023-12-12 22:30:13 +0000

    Fix jinja2 flash filenotfound

--- app.py
+++ app.py
@@ -1,15 +1,28 @@
 import os
 from flask import Flask, render_template 
+import flask
+
+
 #  importing Flask class
 
 app = Flask (__name__) # creating an istance of this class and storing it in a variable called app
 
-@app.route("/") # using root decorator or py notation - a way of wrapping the functions. Root directory is a forward slash /
+@app.route('/') # using root decorator or py notation - a way of wrapping the functions. Root directory is a forward slash /
 def index():
-    return render_template("index.html")
+    return render_template("index.html") # binds to itself. 
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/test")
+def test():
+    test_message = "Hello from Flask!"
+    return render_template("test.html", message=test_message)
+
 
 if __name__ == "__main__":
     app.run(
         host=os.environ.get("IP", "0.0.0.0"), #  looking for ip vaariable if it exists
-        port=int(os.environ.get("PORT", "8080")), # this port works http://172.20.10.3:8080/ 
+        port=int(os.environ.get("PORT", "8081")), # this port works http://172.20.10.3:8080/ 
         debug=True) # WARNING !!!! never have debug as True in a production application!!!!
